d21bc15f92b02eed22e16539fc89aa7661757cc4
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-02-13 18:26:17 +0100

    Now checks and fixes any output folder issues, also now puts the outputted playlists in a folder with the current date

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var readline = require('readline');
-var {google} = require('googleapis');
+var { google } = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
 // If modifying these scopes, delete previously saved credentials
@@ -8,21 +8,27 @@ var OAuth2 = google.auth.OAuth2;
 var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
 var TOKEN_DIR = './credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'project_token.json';
+var save_path = './generated/playlists/'
 
-// Load client secrets from local file.
+
+// Main
+// Load client secrets from local file and run the script.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
 
-  let playlistIds = process.argv.slice(2);
+    if (make_save_directory() == false) return;
 
-  // Get all playlist items from input playlistId's
-  playlistIds.forEach(pl => {
-    authorize(JSON.parse(content), getPlaylist, pl);
-  });
+    let playlistIds = process.argv.slice(2);
+
+    // Get all playlist items from input playlistId's
+    playlistIds.forEach(pl => {
+        authorize(JSON.parse(content), getPlaylist, pl);
+    });
 });
+
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -32,21 +38,22 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback, cbArgs) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client, cbArgs);
-    }
-  });
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client, cbArgs);
+        }
+    });
 }
+
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -56,30 +63,31 @@ function authorize(credentials, callback, cbArgs) {
  * @param {getEventsCallback} callback The callback to call with the authorized client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
     });
-  });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Enter the code from that page here: ', function (code) {
+        rl.close();
+        oauth2Client.getToken(code, function (err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
 }
+
 
 /**
  * Store token to disk be used in later program executions.
@@ -87,18 +95,51 @@ function getNewToken(oauth2Client, callback) {
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
     }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-    if (err) throw err;
-    console.log('Token stored to ' + TOKEN_PATH);
-  });
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) throw err;
+        console.log('Token stored to ' + TOKEN_PATH);
+    });
 }
+
+
+/**
+ * Check if save directory exists and creates it if it does not.
+ * 
+ * @returns {boolean} false if an error occurred or the save folder for this date already exists.
+ */
+function make_save_directory() {
+    // Make the save directory if it does not already exist
+    try {
+        // Make sure the main directory exists
+        if (!fs.existsSync(save_path)) {
+            fs.mkdirSync(save_path, {recursive: true});
+        }
+
+        // Create the instance specific save path
+        const date = new Date();
+        save_path += date.toISOString().split('T')[0] + '/';
+
+        if (!fs.existsSync(save_path)) {
+            fs.mkdirSync(save_path);
+        }
+        else {
+            console.log("A backup folder already exists for today. Delete it if you want to rerun the backup.\nFolder: " + save_path)
+            return false;
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return false;
+    }
+}
+
 
 /**
  * Lists names of items in specified user playlist.
@@ -107,46 +148,47 @@ function storeToken(token) {
  */
 function getPlaylist(auth, playlistId) {
 
-  let itemsCount = 0;
-  let data = {
-    items: []
-  }
-
-  var service = google.youtube('v3');
-
-  service.playlists.list({
-    auth: auth,
-    part: 'snippet',
-    id: playlistId
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
+    let itemsCount = 0;
+    let data = {
+        items: []
     }
 
-    let playlistInfo = response.data.items[0].snippet;
+    var service = google.youtube('v3');
 
-    service.playlistItems.list({
-      auth: auth,
-      part: 'snippet',
-      playlistId: playlistId,
-      maxResults: 50
-    }, (err, response) => {
-      if (err) {
-        console.log('The API returned an error: ' + err);
-        return;
-      }
-      
-      var playlistItems = response.data.items;
-      playlistItems.forEach(item => {
-        data.items.push(item.snippet.title);
-        itemsCount++;
-      });
-  
-      recursePlaylistItemPages(service, auth, response.data.nextPageToken, playlistId, playlistInfo, data, itemsCount);
+    service.playlists.list({
+        auth: auth,
+        part: 'snippet',
+        id: playlistId
+    }, function (err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+
+        let playlistInfo = response.data.items[0].snippet;
+
+        service.playlistItems.list({
+            auth: auth,
+            part: 'snippet',
+            playlistId: playlistId,
+            maxResults: 50
+        }, (err, response) => {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                return;
+            }
+
+            var playlistItems = response.data.items;
+            playlistItems.forEach(item => {
+                data.items.push(item.snippet.title);
+                itemsCount++;
+            });
+
+            recursePlaylistItemPages(service, auth, response.data.nextPageToken, playlistId, playlistInfo, data, itemsCount);
+        });
     });
-  });
 }
+
 
 /**
  * Recurse through consequent pages.
@@ -157,31 +199,31 @@ function getPlaylist(auth, playlistId) {
  * @param {String} playlistId The playlist to iterate. 
  */
 function recursePlaylistItemPages(service, auth, pageToken, playlistId, playlistInfo, data, itemsCount) {
-  if (typeof(pageToken) != 'undefined') {
-    service.playlistItems.list({
-      auth: auth,
-      part: 'snippet',
-      playlistId: playlistId,
-      maxResults: 50,
-      pageToken: pageToken
-    }, (err, response) => {
-      if (err) {
-        console.log('The API returned an error: ' + err);
-        return;
-      }
-      
-      var playlistItems = response.data.items;
-      playlistItems.forEach(item => {
-        data.items.push(item.snippet.title);
-        itemsCount++;
-      });
+    if (typeof (pageToken) != 'undefined') {
+        service.playlistItems.list({
+            auth: auth,
+            part: 'snippet',
+            playlistId: playlistId,
+            maxResults: 50,
+            pageToken: pageToken
+        }, (err, response) => {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                return;
+            }
 
-      recursePlaylistItemPages(service, auth, response.data.nextPageToken, playlistId, playlistInfo, data, itemsCount);
-    });
-  }
-  else {
-    fs.writeFileSync('./generated/playlists/' + '(' + playlistInfo.channelTitle + ') ' + playlistInfo.title + '.json', JSON.stringify(data));
-    console.log('\nDone: (' + playlistInfo.channelTitle + ') ' + playlistInfo.title + ', ' + 'Items: ' + itemsCount + '\n' +
-                playlistId + '\n');
-  }
+            var playlistItems = response.data.items;
+            playlistItems.forEach(item => {
+                data.items.push(item.snippet.title);
+                itemsCount++;
+            });
+
+            recursePlaylistItemPages(service, auth, response.data.nextPageToken, playlistId, playlistInfo, data, itemsCount);
+        });
+    }
+    else {
+        fs.writeFileSync(save_path + '(' + playlistInfo.channelTitle + ') ' + playlistInfo.title + '.json', JSON.stringify(data));
+        console.log('\nDone: (' + playlistInfo.channelTitle + ') ' + playlistInfo.title + ', ' + 'Items: ' + itemsCount + '\n' +
+            playlistId + '\n');
+    }
 }
